17bc69f75b7bd2a0c083632e726c3913de3b5ea7
ratiebareeng <bareengbiz@gmail.com>
2025-06-11 16:16:17 +0200

    Add StringHelper

---
+++ lib/helpers/string_helper.dart
@@ -0,0 +1,26 @@
+class StringHelper {
+  static final StringHelper _instance = StringHelper._internal();
+
+  static StringHelper get instance => _instance;
+
+  factory StringHelper() => _instance;
+
+  StringHelper._internal();
+
+  String formatTimeAgo(DateTime dateTime) {
+    final now = DateTime.now();
+    final difference = now.difference(dateTime);
+
+    if (difference.inDays > 30) {
+      return '${(difference.inDays / 30).floor()} month${(difference.inDays / 30).floor() == 1 ? '' : 's'} ago';
+    } else if (difference.inDays > 0) {
+      return '${difference.inDays} day${difference.inDays == 1 ? '' : 's'} ago';
+    } else if (difference.inHours > 0) {
+      return '${difference.inHours} hour${difference.inHours == 1 ? '' : 's'} ago';
+    } else if (difference.inMinutes > 0) {
+      return '${difference.inMinutes} minute${difference.inMinutes == 1 ? '' : 's'} ago';
+    } else {
+      return 'Just now';
+    }
+  }
+}
