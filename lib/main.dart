7dfb6282b3fb0bca9dcabe2c323b86955fb61d81
Ratie Bareeng <bareengbiz@gmail.com>
2025-06-20 08:31:35 +0300

    Add dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Claude Clone',
         scaffoldMessengerKey: scaffoldMessengerKey,
-        theme: ThemeData.dark().copyWith(
+        darkTheme: ThemeData.dark().copyWith(
           colorScheme: ColorScheme.fromSeed(
               seedColor:
                   Color(0xFFCD7F32), //0xFFCD7F32 // 0xffda7756 // 0xffbd5d3a
@@ -80,6 +80,57 @@ class MyApp extends StatelessWidget {
             filled: true,
             fillColor: Color(0xFF262624),
           ),
+          iconTheme: IconThemeData(color: Colors.blue),
+        ),
+        theme: ThemeData.light().copyWith(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor:
+                  Color(0xFFCD7F32), //0xFFCD7F32 // 0xffda7756 // 0xffbd5d3a
+              primary: Color(0xFFCD7F32),
+              surface: Color(0xFF262624),
+              onSurface: Color(0xFFFAF9F5)),
+          scaffoldBackgroundColor: Color(0xFF262624),
+          cardTheme: CardTheme(
+            color: Color(0xFF262624),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+              side: BorderSide(color: Colors.grey.shade800, width: 0.5),
+            ),
+          ),
+          appBarTheme: AppBarTheme(
+            centerTitle: false,
+            backgroundColor: Color(0xFF262624),
+            elevation: 0,
+            titleTextStyle: GoogleFonts.gideonRoman(
+              color: Colors.grey.shade400,
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          textTheme: GoogleFonts.latoTextTheme(
+            Theme.of(context).textTheme.apply(
+                  bodyColor: Colors.white,
+                  displayColor: Colors.white,
+                ),
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+            hintStyle: TextStyle(color: Colors.grey),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: BorderSide(color: Colors.grey.shade600),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: BorderSide(color: Colors.grey.shade600),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: BorderSide(color: Colors.blue),
+            ),
+            filled: true,
+            fillColor: Color(0xFF262624),
+          ),
+          iconTheme: IconThemeData(color: Colors.blue),
         ),
         home: HomeScreen(),
         debugShowCheckedModeBanner: false,
