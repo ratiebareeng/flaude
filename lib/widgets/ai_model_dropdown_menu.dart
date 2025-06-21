ae3fb6cc19377e5b97a148b7e912253d9e173353
Ratie Bareeng <bareengbiz@gmail.com>
2025-06-21 13:10:41 +0300

    Add AIModelDropDownMenu

---
+++ lib/widgets/ai_model_dropdown_menu.dart
@@ -0,0 +1,60 @@
+import 'package:claude_chat_clone/models/models.dart';
+import 'package:flutter/material.dart';
+
+class AIModelDropDownMenu extends StatefulWidget {
+  final List<AIModel> menuEntries;
+  final AIModel? initialEntry;
+  final Function(AIModel)? onSelected;
+  const AIModelDropDownMenu({
+    super.key,
+    required this.menuEntries,
+    this.initialEntry,
+    this.onSelected,
+  });
+  @override
+  State<AIModelDropDownMenu> createState() => _AIModelDropDownMenuState();
+}
+
+class _AIModelDropDownMenuState extends State<AIModelDropDownMenu> {
+  bool _isOpen = false;
+  AIModel? _selectedModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Focus(
+      onFocusChange: (hasFocus) {
+        setState(() {
+          _isOpen = hasFocus;
+        });
+      },
+      child: DropdownMenu<AIModel>(
+        initialSelection: widget.initialEntry,
+        trailingIcon: Builder(
+          builder: (context) {
+            // final isOpen = MenuController.maybeIsOpenOf(context)!;
+            // Icons.arrow_drop_down Icons.arrow_drop_up
+            return Icon(!_isOpen
+                ? Icons.keyboard_arrow_up_rounded
+                : Icons.keyboard_arrow_down_rounded);
+          },
+        ),
+        dropdownMenuEntries: widget.menuEntries
+            .map((model) => DropdownMenuEntry<AIModel>(
+                  value: model,
+                  label: model.name,
+                ))
+            .toList(),
+        onSelected: (model) {
+          if (model == null || model == _selectedModel) {
+            return; // Avoid unnecessary updates
+          }
+
+          setState(() {
+            _selectedModel = model;
+          });
+          widget.onSelected?.call(model);
+        },
+      ),
+    );
+  }
+}
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,3 +1,4 @@
+export 'ai_model_dropdown_menu.dart';
 export 'create_project_dialog.dart';
 export 'navigation_drawer_widget.dart';
 export 'project_summary_widget.dart';
