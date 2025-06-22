b1e39074bd0a5f9708e3342b066e79be9717ae48
Ratie Bareeng <bareengbiz@gmail.com>
2025-06-22 15:57:57 +0300

    Add ChatItem widget

--- lib/screens/chats_screen.dart
+++ lib/screens/chats_screen.dart
@@ -1,5 +1,6 @@
 import 'package:claude_chat_clone/models/models.dart';
 import 'package:claude_chat_clone/screens/services/chat_service.dart';
+import 'package:claude_chat_clone/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class ChatsScreen extends StatefulWidget {
@@ -84,23 +85,6 @@ class _ChatsScreenState extends State<ChatsScreen> {
 
   void _onNewChatTap() {
     widget.onNewChatPressed?.call();
-  }
-
-  String _formatLastMessageTime(DateTime? timestamp) {
-    if (timestamp == null) return '';
-
-    final now = DateTime.now();
-    final difference = now.difference(timestamp);
-
-    if (difference.inDays > 0) {
-      return '${difference.inDays} day${difference.inDays > 1 ? 's' : ''} ago';
-    } else if (difference.inHours > 0) {
-      return '${difference.inHours} hour${difference.inHours > 1 ? 's' : ''} ago';
-    } else if (difference.inMinutes > 0) {
-      return '${difference.inMinutes} minute${difference.inMinutes > 1 ? 's' : ''} ago';
-    } else {
-      return 'Just now';
-    }
   }
 
   @override
@@ -303,112 +287,8 @@ class _ChatsScreenState extends State<ChatsScreen> {
       padding: EdgeInsets.symmetric(horizontal: 24),
       itemCount: _filteredChats.length,
       itemBuilder: (context, index) {
-        final chat = _filteredChats[index];
-        return _buildChatItem(chat);
+        return ChatItem(chat: _filteredChats[index]);
       },
-    );
-  }
-
-  Widget _buildChatItem(Chat chat) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 8),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: InkWell(
-        onTap: () => _onChatTap(chat),
-        borderRadius: BorderRadius.circular(12),
-        child: Container(
-          padding: EdgeInsets.all(16),
-          child: Row(
-            children: [
-              // Chat Icon
-              Container(
-                width: 40,
-                height: 40,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Icon(
-                  Icons.chat_outlined,
-                  color: Colors.grey[400],
-                  size: 20,
-                ),
-              ),
-
-              SizedBox(width: 12),
-
-              // Chat Details
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // Chat Title
-                    Text(
-                      chat.title,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                      ),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-
-                    SizedBox(height: 4),
-
-                    // Last Message Time
-                    Text(
-                      'Last message ${_formatLastMessageTime(chat.updatedAt)}',
-                      style: TextStyle(
-                        fontSize: 14,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              // Options Menu
-              IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.delete_forever_outlined,
-                    color: Colors.grey,
-                  )),
-              // PopupMenuButton<String>(
-              //   icon: Icon(
-              //     Icons.more_vert,
-              //     color: Colors.grey[400],
-              //     size: 20,
-              //   ),
-              //   color: Color(0xFF3A3A3A),
-              //   onSelected: (value) => _handleChatAction(value, chat),
-              //   itemBuilder: (context) => [
-              //     PopupMenuItem(
-              //       value: 'rename',
-              //       child: Row(
-              //         children: [
-              //           Icon(Icons.edit, color: Colors.white, size: 16),
-              //           SizedBox(width: 8),
-              //           Text('Rename', style: TextStyle(color: Colors.white)),
-              //         ],
-              //       ),
-              //     ),
-              //     PopupMenuItem(
-              //       value: 'delete',
-              //       child: Row(
-              //         children: [
-              //           Icon(Icons.delete, color: Colors.red, size: 16),
-              //           SizedBox(width: 8),
-              //           Text('Delete', style: TextStyle(color: Colors.red)),
-              //         ],
-              //       ),
-              //     ),
-              //   ],
-              // ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 
