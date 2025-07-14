c8e858f266b64d2f8afee67bf1c65a0487722995
ratiebareeng <bareengbiz@gmail.com>
2025-07-14 12:32:21 +0200

    Add MessageBubble

---
+++ lib/ui/widgets/molecules/message_bubble.dart
@@ -0,0 +1,105 @@
+import 'package:claude_chat_clone/data/util/date_time_utils.dart';
+import 'package:claude_chat_clone/domain/models/models.dart';
+import 'package:claude_chat_clone/ui/widgets/atoms/artefact_preview.dart';
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  final Message message;
+  final Function(Map<String, dynamic>?)? viewArtifact;
+  const MessageBubble(
+      {super.key, required this.message, required this.viewArtifact});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: const BoxConstraints(maxWidth: 800),
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (!message.isUser) ...[
+            Container(
+              width: 32,
+              height: 32,
+              decoration: BoxDecoration(
+                color: const Color(0xFFCD7F32),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Center(
+                child: Text(
+                  'C',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 12),
+          ],
+          Expanded(
+            child: Column(
+              crossAxisAlignment: message.isUser
+                  ? CrossAxisAlignment.end
+                  : CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: message.isUser
+                        ? const Color(0xFF3A3A3A)
+                        : const Color(0xFF2F2F2F),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        message.content,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          height: 1.5,
+                        ),
+                      ),
+                      if (message.hasArtifact! && message.artifact != null)
+                        ArtifactPreview(
+                          artifact: message.artifact!,
+                          viewArtifact: viewArtifact,
+                        ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  DateTimeUtils.formatTimestamp(message.timestamp),
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          if (message.isUser) ...[
+            const SizedBox(width: 12),
+            Container(
+              width: 32,
+              height: 32,
+              decoration: BoxDecoration(
+                color: Colors.grey[600],
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Icon(
+                Icons.person,
+                color: Colors.white,
+                size: 18,
+              ),
+            ),
+          ],
+        ],
+      ),
+    );
+  }
+}
