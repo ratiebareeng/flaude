e224d1cb6d000622dcceddc955909d2a4e1b0b0b
ratiebareeng <bareengbiz@gmail.com>
2025-07-14 12:31:53 +0200

    Add DateTimeUtils

---
+++ lib/data/util/date_time_utils.dart
@@ -0,0 +1,17 @@
+class DateTimeUtils {
+  /// Format timestamp for display
+  static String formatTimestamp(DateTime timestamp) {
+    final now = DateTime.now();
+    final difference = now.difference(timestamp);
+
+    if (difference.inMinutes < 1) {
+      return 'Just now';
+    } else if (difference.inMinutes < 60) {
+      return '${difference.inMinutes}m ago';
+    } else if (difference.inHours < 24) {
+      return '${difference.inHours}h ago';
+    } else {
+      return '${difference.inDays}d ago';
+    }
+  }
+}
