fc7527789f02334e6a6e51ec7eec592021dec7f5
ratiebareeng <bareengbiz@gmail.com>
2025-08-03 19:00:00 +0200

    Add constants

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,10 @@
+// lib/core/constants/constants.dart
+
+/// Barrel file for all application constants
+///
+/// This file exports all constant classes to provide a single import point
+/// for accessing application-wide constants.
+library;
+
+export 'api_constants.dart';
+export 'app_constants.dart';
