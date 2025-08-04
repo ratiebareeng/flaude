44f8fb0993a4ec93a949ee5818fc40f01395480e
ratiebareeng <bareengbiz@gmail.com>
2025-08-04 22:22:15 +0200

    Update domain barrel

--- lib/domain/domain.dart
+++ lib/domain/domain.dart
@@ -1,15 +1,13 @@
 /// Domain layer barrel file
 ///
-/// This file exports all domain layer components including entities, 
+/// This file exports all domain layer components including entities,
 /// repository interfaces, and use cases for easy importing throughout the app.
 /// The domain layer represents the core business logic and is framework-independent.
 library;
 
 // Core business entities
 export 'entities/entities.dart';
-
 // Repository contracts/interfaces
 export 'repositories/repositories.dart';
-
 // Use cases will be exported here when implemented
-// export 'usecases/usecases.dart';
+export 'usecases/usecases.dart';
